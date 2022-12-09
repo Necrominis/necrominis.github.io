bc855403be8a76d75557410f53226eb9bc64edbb
Tuckertcs <tucker.sandin@gmail.com>
2022-12-09 12:47:46 -0600

    Change post date format from DD-MM-YYYY to YYYY-MM-DD

--- .builder/data.py
+++ .builder/data.py
@@ -276,21 +276,20 @@ data = {
 			"title": "Gallery",
 			"type": "gallery",
 			"pages": [
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
-				"10-06-2022",
+				"2022-06-10",
+				"2022-06-10",
+				"2022-06-10",
+				"2022-06-10",
+				"2022-06-10",
+				"2022-06-10",
+				"2022-06-10",
+				"2022-06-10",
+				"2022-06-10",
+				"2022-06-10",
 			],
 		},
 		# ============================================================================ #
-		"10-06-2022": {
+		"2022-06-10": {
 			"title": "Vampire Necromancer",
 			"type": "post",
 			"properties": {
@@ -309,10 +308,10 @@ data = {
 				],
 			},
 			"images": [
-				"necrominis-10-06-2022-raw-1.jpeg",
-				"necrominis-10-06-2022-raw-2.jpeg",
-				"necrominis-10-06-2022-raw-3.jpeg",
-				"necrominis-10-06-2022-raw-4.jpeg",
+				"necrominis-2022-06-10-raw-1.jpeg",
+				"necrominis-2022-06-10-raw-2.jpeg",
+				"necrominis-2022-06-10-raw-3.jpeg",
+				"necrominis-2022-06-10-raw-4.jpeg",
 			],
 			"paragraphs": [
 				f"""{FP_}A {MN_}Soulblight Gravelord Vampire{_MN} painted with a necromancy magic scheme.{_FP}""",
@@ -321,7 +320,7 @@ data = {
 				f"""Seeing the rest of the model still black made me realize a dark outfit would fit the best and contrast with the light skin. The armor pieces remained black, but were highlighted toward dark purple and edge highlighted with a similar purple-grey mix from the skin's base coat. The rest of the straps, the mace, and the hair were highlighted similarly with dark to medium greys, as I wanted to avoid using metallic paints for this mini.""",
 				f"""The last details were a few gems and a bottle on the vampire's waist. These were painted with the two teal colors and the lime green, to remain coherent with the base.""",
 				f"""Finally, I airbrushed the two teal colors onto the model from below, to give the effect that the green light from the base is lighting up the vampire from below. Then I gave it a clear coat of gloss, for protective purposes, and matte for the final finish.""",
-				f"""All-in-all, I think this might be my best miniature yet, in terms of the color coherency, the skin shading, and the glow effect. One of my {A_}https://necrominis.github.io/post/10-06-2022/{_A_}previous miniatures{_A} had a better paint job for the skin, but that was with an airbrush; this miniature's skin was painted by hand, and this one has higher contrast. I do think the wings' spots could be redone, though. I wanted to highlight the horizontal folds in the wings, but they weren't defined enough to catch the edge of my brush, and were too small to highlight by hand. Overall, though, I think this miniature turned out beautifully.""",
+				f"""All-in-all, I think this might be my best miniature yet, in terms of the color coherency, the skin shading, and the glow effect. One of my {A_}https://necrominis.github.io/post/2022-03-15/{_A_}previous miniatures{_A} had a better paint job for the skin, but that was with an airbrush; this miniature's skin was painted by hand, and this one has higher contrast. I do think the wings' spots could be redone, though. I wanted to highlight the horizontal folds in the wings, but they weren't defined enough to catch the edge of my brush, and were too small to highlight by hand. Overall, though, I think this miniature turned out beautifully.""",
 			],
 			"paints-used": [
 				{
