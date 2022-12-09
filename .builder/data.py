63f2aa05408da0315a91d3a4c234466c37809545
Tuckertcs <tucker.sandin@gmail.com>
2022-12-09 12:48:57 -0600

    Reflect YYYY-MM-DD change in comments as well

--- .builder/data.py
+++ .builder/data.py
@@ -1,38 +1,38 @@
 # Make sure to always use trailing commas!
-# Dates are DD-MM-YYYY!
+# Dates are YYYY-MM-DD!
 
 
 
 
 
-# First paragraph f"{FP_}text{_FP}".
+# First paragraph f"""{FP_}text{_FP}""".
 FP_ = """<mark class="first-paragraph">"""
 _FP = """</mark>"""
 
-# Mini name f"{MN_}text{_MN}".
+# Mini name f"""{MN_}text{_MN}"""..
 MN_ = """<mark class="mini-name">"""
 _MN = """</mark>"""
 
-# Tooltip f"{TT_}text{_TT_}description{_TT}".
+# Tooltip f"""{TT_}text{_TT_}description{_TT}""".
 TT_ = """<mark class="tooltip">"""
 _TT_ = """<span class="tip">"""
 _TT = """</span></mark>"""
 
-# Link f"{A_}url{_A_}text{_A}".
+# Link f"""{A_}url{_A_}text{_A}""".
 A_ = """<a href=\""""
 _A_ = """\">"""
 _A = """</a>"""
 
-# Italic f"{I_}text{_i}".
+# Italic f"""{I_}text{_i}""".
 I_ = """<i>"""
 _I = """</i>"""
 
-# Italic link f"{IA_}url{_IA_}text{_IA}".
+# Italic link f"""{IA_}url{_IA_}text{_IA}""".
 IA_ = f"""{I_}{A_}"""
 _IA_ = f"""{_A_}"""
 _IA = f"""{_I}{_A}"""
 
-# Bold f"{B_}text{_B}".
+# Bold f"""{B_}text{_B}""".
 B_ = """<b>"""
 _B = """</b>"""
 
